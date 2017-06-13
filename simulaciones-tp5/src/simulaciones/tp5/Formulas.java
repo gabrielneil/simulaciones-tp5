e6e5f598232e99e40ada03947a6ad95dd1506b6e
Gabriel Neil <gabriel.neil@mercadolibre.com>
2017-06-12 23:01:05 -0300

    todas las formulas done

--- simulaciones-tp5/src/simulaciones/tp5/Formulas.java
+++ simulaciones-tp5/src/simulaciones/tp5/Formulas.java
@@ -13,27 +13,29 @@ public class Formulas {
 
     public static double tiempoCompraTicket(float tiempoCompra, float random) {
 
-        return (-tiempoCompra) * Math.log(1 - random);
+        return (-tiempoCompra) * Math.log(1 - random)/60;
     }
 
+    //Exp negativa, devuelve en minutos
     public static double tiempoEntregaPedido(float tiempoCompra, float random) {
-
-        return (-tiempoCompra) * Math.log(1 - random);
+        return ((-tiempoCompra) * Math.log(1 - random)) / 60;
     }
-    
+
     //box-muller.
     //Tiempo de llegada entre clientes en minutos, 
     //recibe como parametro la media y la desviacion en segundos y los randoms para box muller
     //retorna el tiempo de llegada entre cliente en MINUTOS
-    public static double llegadaCliente(float rnd1, float rnd2, float media, float desviacion){
-        return (((Math.sqrt(-2*Math.log(rnd1))*Math.cos(2*Math.PI*rnd2))*desviacion)+media)/60;
+    public static double llegadaCliente(float rnd1, float rnd2, float media, float desviacion) {
+        return (((Math.sqrt(-2 * Math.log(rnd1)) * Math.cos(2 * Math.PI * rnd2)) * desviacion) + media) / 60;
     }
-    
-    public static double tiempoUtilizacionMesa(float rnd){
-        return 10+(rnd*10);
+
+    //devuelve en minutos
+    public double tiempoConsumicion(int a, int b,float rnd) {
+        return (a-b)+(rnd*((a+b)-(a-b)));
     }
-    
-//    public double tiempoConsumición(){
-//    
-//    }
+
+    //devuelve en minutos
+    public static double tiempoUtilizacionMesa(int a, int b, float rnd) {
+        return (a-b)+(rnd*((a+b)-(a-b)));
+    }
 }

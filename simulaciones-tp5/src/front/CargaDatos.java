912d088513172746fdffc5e446fb2d968123aef9
Gabriel Neil <gabriel.neil@mercadolibre.com>
2017-07-02 11:28:05 -0300

    fixs!!

--- simulaciones-tp5/src/front/CargaDatos.java
+++ simulaciones-tp5/src/front/CargaDatos.java
@@ -270,7 +270,7 @@ public class CargaDatos extends javax.swing.JFrame {
         // TODO add your handling code here:
         if (Integer.parseInt(desde_txt.getText()) >= 0 && Integer.parseInt(hasta_txt.getText()) <= 60 && Integer.parseInt(hasta_txt.getText()) > Integer.parseInt(desde_txt.getText())) {
 
-            if (Integer.parseInt(media_txt.getText()) > 0 && Integer.parseInt(desviacion_txt.getText()) > 0 && Integer.parseInt(entranAComprar_txt.getText()) > 0 && Integer.parseInt(entranAMesa_txt.getText()) > 0 && Integer.parseInt(sientaEnMesa_txt.getText()) > 0 && Integer.parseInt(seRetira_txt.getText()) > 0) {
+            if (Integer.parseInt(media_txt.getText()) >= 0 && Integer.parseInt(desviacion_txt.getText()) >= 0 && Integer.parseInt(entranAComprar_txt.getText()) >= 0 && Integer.parseInt(entranAMesa_txt.getText()) >= 0 && Integer.parseInt(sientaEnMesa_txt.getText()) >= 0 && Integer.parseInt(seRetira_txt.getText()) >= 0) {
                 controller.valoresCargaTiempos(cargaTiempos.getTiempoTicket_txt(), cargaTiempos.getTiempoEspera_txt(), cargaTiempos.getTiempoConsumicion1_txt(), cargaTiempos.getTiempoConsumicion2_txt(), cargaTiempos.getTiempoUtilizacionMesa1_txt(), cargaTiempos.getTiempoUtilizacionMesa2_txt());
                 controller.valoresCargaDatos(Integer.parseInt(media_txt.getText()), Integer.parseInt(desviacion_txt.getText()), Integer.parseInt(entranAComprar_txt.getText()), Integer.parseInt(entranAMesa_txt.getText()), Integer.parseInt(sientaEnMesa_txt.getText()), Integer.parseInt(seRetira_txt.getText()), Integer.parseInt(desde_txt.getText()), Integer.parseInt(hasta_txt.getText()));
                 this.setVisible(false);
